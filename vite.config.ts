7d6ba567a56a24f71b28eea78d2c803e9cc3b538
Patrick Nassar <patrick.nassar@solactive.com>
2025-04-19 13:42:40 +0200

    Initialize project

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      name: 'MyReactComponents',
+      fileName: (format) => `my-react-components.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+});
